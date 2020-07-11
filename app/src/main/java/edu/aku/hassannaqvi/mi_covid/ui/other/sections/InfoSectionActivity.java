c4791e84973e8812b16c5490a05dfb395d165b84
hassan.naqvi <shznaqvi@gmail.com>
2020-07-11 17:57:11 +0500

    - pushing with errors (work in progress)

---
+++ app/src/main/java/edu/aku/hassannaqvi/mi_covid/ui/other/sections/InfoSectionActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.mi_covid.ui.other.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.mi_covid.R;
+
+public class InfoSectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_section);
+    }
+}
