5ef0839fd0298a4df81a408cf4b571b9b4dd2632
muhammad.fazal <fazal.abbas4@gmail.com>
2020-07-13 14:41:50 +0500

    Add section E and F layout

---
+++ app/src/main/java/edu/aku/hassannaqvi/mi_covid/ui/sections/SectionFActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.mi_covid.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.mi_covid.R;
+
+public class SectionFActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_f);
+    }
+}
