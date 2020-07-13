8b2f7d6fffcc7286c3c26144ce583dad5515cc86
muhammad.fazal <fazal.abbas4@gmail.com>
2020-07-13 14:31:44 +0500

    Add section D layout

---
+++ app/src/main/java/edu/aku/hassannaqvi/mi_covid/ui/sections/SectionDActivity.java
@@ -0,0 +1,14 @@
+package edu.aku.hassannaqvi.mi_covid.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SectionDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_d);
+    }
+}
