cb4d538706da16998d99fc162003f5dea91569e0
Muhammad Hussain <ahsanpervez74@gmail.com>
2020-07-15 15:42:02 +0500

    SectionB Change change input type for numeric inputs

--- app/src/main/java/edu/aku/hassannaqvi/mi_covid/ui/sections/SectionBActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/mi_covid/ui/sections/SectionBActivity.java
@@ -6,16 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import com.validatorcrawler.aliazaz.Clear;
 import com.validatorcrawler.aliazaz.Validator;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import edu.aku.hassannaqvi.mi_covid.R;
 import edu.aku.hassannaqvi.mi_covid.contracts.FormsContract;
 import edu.aku.hassannaqvi.mi_covid.core.DatabaseHelper;
@@ -34,6 +33,7 @@ public class SectionBActivity extends AppCompatActivity {
         //bi.setCallback(this);
         setupSkip();
     }
+
 
     private void setupSkip() {
         //b01
